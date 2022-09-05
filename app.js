075a6f23baf04d542be9b4d9e67465f30ec1813e
RoninMirrorImage <spina.amedeo.pio@gmail.com>
2022-09-05 03:18:26 +0200

    algoritmo quasi ultimato

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
-let target;
+const UnixConverter = (UnixTime) => {
+  const milliseconds = UnixTime * 1000;
+  const timeObject = new Date(milliseconds);
+  const convertedTime = timeObject.toLocaleString();
+  return convertedTime;
+};
+
+const addNews = (news) => {
+  const divNews = document.createElement("div");
+  divNews.className = "news";
+  const h3 = document.createElement("h3")
+  h3.textContent = news.title;
+  divNews.appendChild(h3);
+  const divNewsUrl = document.createElement("div");
+  divNewsUrl.className = "newsUrl";
+  const anchor = document.createElement("a");
+  anchor.href = news.url;
+  anchor.textContent = news.url;
+  divNewsUrl.appendChild(anchor);
+  divNews.appendChild(divNewsUrl);
+  const divNewsBy = document.createElement("div");
+  divNewsBy.className = "divNewsBy";
+  divNewsBy.textContent = news.by;
+  divNews.appendChild(divNewsBy);
+  const divNewsRelease = document.createElement("div");
+  divNewsRelease.className = "divNewsRelease";
+  divNewsRelease.textContent = UnixConverter(news.time);
+  divNews.appendChild(divNewsRelease);
+  allNews.appendChild(divNews);
+};
+
+const getNews = () => {
+  newsAry.forEach(idArt => {
+  fetch("https://hacker-news.firebaseio.com/v0/item/" + idArt + ".json?print=pretty")
+  .then((response) => response.json())
+  .then((data) => {addNews(data)});
+  });
+};
+
+let newsAry = [];
+let allArticles;
+const allNews = document.querySelector(".allNews");
 
 fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-  .then((response) => response.json())
-  .then((data) => {target = data; console.log(data)});
+.then((response) => response.json())
+.then((data) => {
+  Object.values(data).map(value => {
+    newsAry.push(value);
+  });
+});
 
-let index = 5467;
-
-fetch("https://hacker-news.firebaseio.com/v0/item/"+ index + ".json?print=pretty")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+getNews();
