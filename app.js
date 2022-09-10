57f5530ee7620c0055d3a052c29bc8b97755b26d
RoninMirrorImage <102737912+RoninMirrorImage@users.noreply.github.com>
2022-09-10 16:45:34 +0200

    sistemare addNews

--- app.js
+++ app.js
@@ -6,54 +6,57 @@ const UnixConverter = (UnixTime) => {
 };
 
 const addNews = (news) => {
-  const divNews = document.createElement("div");
-  divNews.className = "news";
-  const h3 = document.createElement("h3")
-  h3.textContent = news.title;
-  divNews.appendChild(h3);
-  const divNewsUrl = document.createElement("div");
-  divNewsUrl.className = "newsUrl";
-  const anchor = document.createElement("a");
-  anchor.href = news.url;
-  anchor.textContent = news.url;
-  divNewsUrl.appendChild(anchor);
-  divNews.appendChild(divNewsUrl);
-  const divNewsBy = document.createElement("div");
-  divNewsBy.className = "divNewsBy";
-  divNewsBy.textContent = news.by;
-  divNews.appendChild(divNewsBy);
-  const divNewsRelease = document.createElement("div");
-  divNewsRelease.className = "divNewsRelease";
-  divNewsRelease.textContent = UnixConverter(news.time);
-  divNews.appendChild(divNewsRelease);
-  const allNews = document.querySelector(".allNews");
-  allNews.appendChild(divNews);
+  console.log(news);
+  Object.values(news).map(trg => {
+    console.log(trg)
+    console.log("ciao");
+    const divNews = document.createElement("div");
+    divNews.className = "news";
+    const h3 = document.createElement("h3")
+    h3.textContent = trg.title;
+    divNews.appendChild(h3);
+    const divNewsUrl = document.createElement("div");
+    divNewsUrl.className = "newsUrl";
+    const anchor = document.createElement("a");
+    anchor.href = trg.url;
+    anchor.textContent = trg.url;
+    divNewsUrl.appendChild(anchor);
+    divNews.appendChild(divNewsUrl);
+    const divNewsBy = document.createElement("div");
+    divNewsBy.className = "divNewsBy";
+    divNewsBy.textContent = trg.by;
+    divNews.appendChild(divNewsBy);
+    const divNewsRelease = document.createElement("div");
+    divNewsRelease.className = "divNewsRelease";
+    divNewsRelease.textContent = UnixConverter(news.time);
+    divNews.appendChild(divNewsRelease);
+    const allNews = document.querySelector(".allNews");
+    allNews.appendChild(divNews);
+  });
 };
 
 const getNews = (idsAry) => {
   const newsAry = []
-  Object.values(idsAry).map((news) => {
-    console.log(idsAry);
-    fetch("https://hacker-news.firebaseio.com/v0/item/" + idnews + ".json?print=pretty")
+  Object.values(idsAry).map( async (id) => {
+    await fetch("https://hacker-news.firebaseio.com/v0/item/" + id + ".json?print=pretty")
     .then((res) => res.json())
-    .then((data) => {newsAry.push(data); console.log(newsAry)})
+    .then((data) => {newsAry.push(data);})
     .catch((err) => console.log(err))
   });
-  console.log(newsAry);
   return newsAry;
 }
 
-const fetchIdNews = () => {
-  fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+const fetchIdNews = async() => {
+  let ids;
+  await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
   .then((res) => res.json())
-  .then((data) => {return data})
-  .catch((err) => console.log(err))
+  .then((data) => {ids = data;})
+  .catch((err) => console.log(err));
+  return ids;
 }
 
 (async () => {
-
   const idsNewsAry = await fetchIdNews();
   const allNewsAry = await getNews(idsNewsAry);
   addNews(allNewsAry);
-
 })();
